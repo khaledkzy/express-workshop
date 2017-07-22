87293fb6c96a5d4d0376bc8fbb43f64b93811ccf
khaledkzy <khaledkzy@gmail.com>
2017-07-22 17:05:44 +0100

    I have added Asynchronous Method

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const fs = require('fs')
 app.use(express.static("public"));
 const formidable = require('express-formidable')
 app.use(formidable());
-
-
-
+app.get('/get-posts', function (req, res) {
+  res.sendFile(__dirname + '/data/posts.json')
+})
 app.post('/create-post', function (req, res) {
   //res.json(req.fields)
+  /* Using Synchronus Method
   const filePath = __dirname + '/data/posts.json';
   //console.log(req.fields.blogpost);
   const postsContent = fs.readFileSync(filePath);
@@ -18,25 +19,23 @@ app.post('/create-post', function (req, res) {
   //console.log(posts);
   fs.writeFileSync(filePath, JSON.stringify(posts))
   res.send(200, posts)
- 
-             
-
-
-
-
-  //    fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-  //     console.log(file);
-  // });
-
-  //   fs.writeFile(filePath, JSON.stringify(req.fields), function () {
-  //     res.send(200)
-  // });
-
-  //console.log('the post')
-  //console.log(req.fields);
-
+*/
+// Using Synchronus Method
+  const filepath = __dirname + '/data/posts.json';
+  fs.readFile(filepath, function (err, data) {
+    const postsContent = JSON.parse(data);
+    const timeStamp = Date.now();
+    postsContent[timeStamp] = req.fields.blogpost;
+    fs.writeFile(filepath, JSON.stringify(postsContent, null, 2), function () {
+      res.send(JSON.stringify(postsContent, null, 2))
+    });
+  });
 });
 
 app.listen(3000, function () {
   console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+app.get("/", function (req, res) {
+res.send('this is my fucntion')
 });
